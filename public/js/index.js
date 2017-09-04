b0b0e6e6e23573fb56b583950d896c509afb0413
Cullen <cpsulli1991@gmail.com>
2017-09-04 16:56:33 -0500

    re-commiting with index.js

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+
+socket.on('connect', function () {
+  console.log('connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('disconnected from server')
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
+
+
+// socket.on('newMessage', function () {
+//
+// });
